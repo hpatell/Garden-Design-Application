5a2b3400d75594d1f6b2c56773528ae247f8a6a7
Ronald Abraham <ronjabra@udel.edu>
2021-04-15 10:37:58 -0400

    added save, theme, and adjust budget

--- src/main/java/settingsScreen.java
+++ src/main/java/settingsScreen.java
@@ -3,11 +3,19 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class settingsScreen extends Screen {
 
     Button exitButton = new Button("Exit");
-    
+    Button saveButton = new Button("Save");
+    Label adjBudget = new Label("Adj Budget");
+	TextField adjBudjgettf = new TextField();
+	Label theme = new Label("Theme: ");
+	Button lightButton = new Button("Light");
+	Button darkButton = new Button("Dark");
+	
 	Image backgroundimg = new Image(getClass().getResourceAsStream("/gear.png"));
 	ImageView backgroundimgview = new ImageView(backgroundimg);
 
@@ -16,28 +24,35 @@ public class settingsScreen extends Screen {
 
         exitButton.setTranslateX(0);
         exitButton.setTranslateY(50);
+        
+        saveButton.setTranslateX(100);
+        saveButton.setTranslateY(50);
+        
+        adjBudget.setTranslateX(-100);
+        adjBudget.setTranslateY(-100);
+                
+        adjBudjgettf.setTranslateX(0);
+        adjBudjgettf.setTranslateY(-100);
+        
+        adjBudjgettf.setMaxWidth(100);
+        
+        theme.setTranslateX(-50);
+        theme.setTranslateY(-200);
+        
+        lightButton.setTranslateX(0);
+        lightButton.setTranslateY(-200);
+        
+        darkButton.setTranslateX(50);
+        darkButton.setTranslateY(-200);
 
 
         StackPane layout = new StackPane();
-        layout.getChildren().addAll(backgroundimgview, exitButton);
+        layout.getChildren().addAll(backgroundimgview, exitButton, saveButton, adjBudget, adjBudjgettf, theme, lightButton, darkButton);
         scene = new Scene(layout, canvasWidth, canvasHeight);
 
         exitButton.setOnAction(e -> view.switchPage(PagesEnum.PreviousScreen));
     }
 
     public void save() {
-
-    }
-
-    public void adjBudgetTF(){
-
-    }
-
-    public void changeTheme(){
-
-    }
-
-    public void finish(){
-
     }
 }
