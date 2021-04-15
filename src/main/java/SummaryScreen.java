4ec4d69c4ddb385ab31a49b5b6c097720fe1af87
kushhp <kushhp@udel.edu>
2021-04-15 18:03:49 -0400

    progress bar example

--- src/main/java/SummaryScreen.java
+++ src/main/java/SummaryScreen.java
@@ -1,21 +1,37 @@
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.scene.control.ProgressBar;
+
 
 public class SummaryScreen extends Screen {
 	
-	String[] plantName;
-	int leps;
-	int totalLeps;
-	int totalPlants;
-
+	ProgressBar budgetbar = new ProgressBar(.40);
 	
-	
+	Button back = new Button("Return to Plot");
+	Button save = new Button("Save");
 	
 	public SummaryScreen(View v) {
 		super(v);
+		
     	StackPane layout = new StackPane();
-    	layout.getChildren();	
-    	scene = new Scene(layout, 500, 800);
+    	
+    	budgetbar.setTranslateX(0);
+    	budgetbar.setTranslateY(-250);
+    	
+    	budgetbar.setMaxHeight(40);
+    	budgetbar.setMaxWidth(600);
+    	
+    	save.setTranslateX(50);
+    	save.setTranslateY(200);
+    	
+    	back.setTranslateX(-50);
+    	back.setTranslateY(200);
+    	
+    	layout.getChildren().addAll(back, save, budgetbar);
+    	
+    	scene = new Scene(layout, canvasWidth, canvasHeight);
 	}
 
 	public void budgetBar() {
@@ -29,5 +45,4 @@ public class SummaryScreen extends Screen {
 	public void saveFileButton() {
 		
 	}
-	//sample
 }
