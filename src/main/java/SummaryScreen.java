1b2df3c6b61e2e9c62c484615b888e3094fd3e4e
kushhp <kushhp@udel.edu>
2021-04-15 18:33:24 -0400

    abc

--- src/main/java/SummaryScreen.java
+++ src/main/java/SummaryScreen.java
@@ -1,6 +1,9 @@
+import java.awt.Font;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.control.ProgressBar;
 
@@ -11,6 +14,18 @@ public class SummaryScreen extends Screen {
 	
 	Button back = new Button("Return to Plot");
 	Button save = new Button("Save");
+	
+	Text budgettext = new Text("Budget");
+	Text plantstext = new Text("Plants In Garden");
+	Text lepstext = new Text("Leps In Garden");
+	
+	Text FragariaStrawberry = new Text("Fragaria (Strawberry)");
+	Text HelianthusSunflower = new Text("Helianthus (Sunflower)");
+	Text SalixWillow = new Text("Salix (Willow)");
+	
+	Text FragariaStrawberrylep = new Text("63");
+	Text HelianthusSunflowerlep = new Text("57");
+	Text SalixWillowlep = new Text("292");
 	
 	public SummaryScreen(View v) {
 		super(v);
@@ -23,26 +38,41 @@ public class SummaryScreen extends Screen {
     	budgetbar.setMaxHeight(40);
     	budgetbar.setMaxWidth(600);
     	
-    	save.setTranslateX(50);
+    	budgettext.setTranslateX(0);
+    	budgettext.setTranslateY(-300);
+    	
+    	save.setTranslateX(150);
     	save.setTranslateY(200);
     	
     	back.setTranslateX(-50);
     	back.setTranslateY(200);
     	
-    	layout.getChildren().addAll(back, save, budgetbar);
+    	plantstext.setTranslateX(-150);
+    	plantstext.setTranslateY(-100);
+    	
+    	lepstext.setTranslateX(75);
+    	lepstext.setTranslateY(-100);
+    	
+    	FragariaStrawberry.setTranslateX(-150);
+    	FragariaStrawberry.setTranslateY(-50);
+    	
+    	FragariaStrawberrylep.setTranslateX(75);
+    	FragariaStrawberrylep.setTranslateY(-50);
+    	
+    	HelianthusSunflower.setTranslateX(-150);
+    	HelianthusSunflower.setTranslateY(0);
+    	
+    	HelianthusSunflowerlep.setTranslateX(75);
+    	HelianthusSunflowerlep.setTranslateY(0);
+    	
+    	FragariaStrawberry.setTranslateX(-150);
+    	FragariaStrawberry.setTranslateY(50);
+    	
+    	SalixWillowlep.setTranslateX(75);
+    	SalixWillowlep.setTranslateY(50);
+    	
+    	layout.getChildren().addAll(back, save, budgetbar, budgettext, plantstext, lepstext, FragariaStrawberry, FragariaStrawberrylep);
     	
     	scene = new Scene(layout, canvasWidth, canvasHeight);
-	}
-
-	public void budgetBar() {
-		
-	}
-	
-	public void exitButton() {
-		
-	}
-	
-	public void saveFileButton() {
-		
 	}
 }
