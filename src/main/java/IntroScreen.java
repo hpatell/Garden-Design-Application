1eb9a13596404e942294fe39b196f46d959a09c3
Himanshu Patel <hpatell@udel.edu>
2021-04-15 18:17:27 -0400

    added settings and IntroScreen

--- src/main/java/IntroScreen.java
+++ src/main/java/IntroScreen.java
@@ -19,38 +19,71 @@ import javafx.event.EventHandler;
 
 public class IntroScreen extends Screen {
 	
-	Button importSCbutton = new Button("Import Garden");
-	Button guideButton = new Button("Guide");
-	Button createGardenButton = new Button("Create Garden");
-	Button settingsButton = new Button("Settings");
-	Image backgroundimg = new Image(getClass().getResourceAsStream("/introbackground.PNG"));
-	ImageView backgroundimgview = new ImageView(backgroundimg);
+	Image backgroundIMG = new Image(getClass().getResourceAsStream("/introbackground.png"));
+	ImageView backgroundIV = new ImageView(backgroundIMG);
+	
+	StackPane layout = new StackPane();
 	
 	public IntroScreen(View v) {
 		super(v);
     	
-    	importSCbutton.setTranslateX(0);
-    	importSCbutton.setTranslateY(100);
-    	
-    	createGardenButton.setTranslateX(0);
-    	createGardenButton.setTranslateY(50);
-    	
-    	guideButton.setTranslateX(0);
-    	guideButton.setTranslateY(150);
-    	
-    	settingsButton.setTranslateX(450);
-    	settingsButton.setTranslateY(-375);
-    	
-    	
-    	StackPane layout = new StackPane();
+		backgroundIV.setPreserveRatio(true);
+		backgroundIV.setFitHeight(canvasHeight);
+		backgroundIV.setFitWidth(canvasWidth);
+		
+		layout.getChildren().add(backgroundIV);
+		
+		startButton();
+		guideButton();
+		importButton();
+		settingsButton();
+		
     	layout.setMaxHeight(canvasHeight);
     	layout.setMaxWidth(canvasHeight);
-    	layout.getChildren().addAll(backgroundimgview, guideButton, importSCbutton, createGardenButton, settingsButton);
-    	scene = new Scene(layout, canvasWidth, canvasHeight);
     	
-    	importSCbutton.setOnAction(e -> view.switchPage(PagesEnum.ImportScreen));
-    	guideButton.setOnAction(e -> view.switchPage(PagesEnum.GuideScreen));
-    	createGardenButton.setOnAction(e -> view.switchPage(PagesEnum.InitializationScreen));
-    	settingsButton.setOnAction(e -> view.switchPage(PagesEnum.SettingsScreen));
+    	scene = new Scene(layout, canvasWidth, canvasHeight);
 	}
+	
+	public void startButton()
+	{
+		 Button createGardenButton = new Button("Create Garden");
+		 createGardenButton.setTranslateX(0);
+	     createGardenButton.setTranslateY(50);
+	     createGardenButton.setOnAction(e -> view.switchPage(PagesEnum.InitializationScreen));
+	     layout.getChildren().add(createGardenButton);
+	}
+	
+	public void guideButton()
+	{
+		Button guideButton = new Button("Create Garden");
+		guideButton.setTranslateX(0);
+	    guideButton.setTranslateY(150);
+	    guideButton.setOnAction(e -> view.switchPage(PagesEnum.GuideScreen));
+	    layout.getChildren().add(guideButton);
+	}
+	 
+	public void importButton()
+	{
+		Button importbutton = new Button("Import Garden");
+		importbutton.setTranslateX(0);
+		importbutton.setTranslateY(100);
+		importbutton.setOnAction(e -> view.switchPage(PagesEnum.ImportScreen));
+		layout.getChildren().add(importbutton);
+	}
+	 
+	public void settingsButton() 
+	{
+		Button settingsButton = new Button("Settings");
+	    Image gear = new Image(getClass().getResourceAsStream("/gear.png"));
+	    ImageView gearIV = new ImageView(gear);
+	    gearIV.setPreserveRatio(true);
+	    gearIV.setFitHeight(20);
+	    gearIV.setFitWidth(20);
+	    settingsButton.setGraphic(gearIV);
+	    settingsButton.setTranslateX(450);
+	    settingsButton.setTranslateY(-375);
+	    settingsButton.setOnAction(e -> view.switchPage(PagesEnum.SettingsScreen));
+	    layout.getChildren().add(settingsButton);
+    }
+	
 }
--- src/main/java/settingsScreen.java
+++ src/main/java/settingsScreen.java
@@ -1,5 +1,6 @@
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
@@ -7,52 +8,86 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class settingsScreen extends Screen {
-
-    Button exitButton = new Button("Exit");
-    Button saveButton = new Button("Save");
-    Label adjBudget = new Label("Adj Budget");
-	TextField adjBudjgettf = new TextField();
-	Label theme = new Label("Theme: ");
-	Button lightButton = new Button("Light");
-	Button darkButton = new Button("Dark");
 	
-	Image backgroundimg = new Image(getClass().getResourceAsStream("/gear.png"));
-	ImageView backgroundimgview = new ImageView(backgroundimg);
+	StackPane layout = new StackPane();
 
     public settingsScreen(View v) {
         super(v);
 
-        exitButton.setTranslateX(0);
-        exitButton.setTranslateY(50);
+        Label title = new Label("Settings");
+        title.setMinWidth(100);
+        title.setMinHeight(100);
+        title.setTranslateX(0);
+        title.setTranslateY(-200);
+        title.setMaxSize(100, 100);
+        layout.getChildren().add(title);
         
-        saveButton.setTranslateX(100);
-        saveButton.setTranslateY(50);
+        exitButton();
+        adjBudgetTF();
+        changeTheme();
+        saveButton();
+        finish();
         
-        adjBudget.setTranslateX(-100);
-        adjBudget.setTranslateY(-100);
-                
-        adjBudjgettf.setTranslateX(0);
-        adjBudjgettf.setTranslateY(-100);
-        
-        adjBudjgettf.setMaxWidth(100);
-        
-        theme.setTranslateX(-50);
-        theme.setTranslateY(-200);
-        
-        lightButton.setTranslateX(0);
-        lightButton.setTranslateY(-200);
-        
-        darkButton.setTranslateX(50);
-        darkButton.setTranslateY(-200);
-
-
-        StackPane layout = new StackPane();
-        layout.getChildren().addAll(backgroundimgview, exitButton, saveButton, adjBudget, adjBudjgettf, theme, lightButton, darkButton);
+        //layout.getChildren().addAll(exitButton, saveButton, adjBudget, adjBudjgettf, theme, lightButton, darkButton);
         scene = new Scene(layout, canvasWidth, canvasHeight);
-
+    }
+    
+    public void exitButton() 
+    {
+    	Button exitButton = new Button("Exit");
+    	Image gear = new Image(getClass().getResourceAsStream("/gear.png"));
+    	ImageView gearIV = new ImageView(gear);
+    	gearIV.setPreserveRatio(true);
+    	gearIV.setFitHeight(20);
+    	gearIV.setFitWidth(20);
+    	exitButton.setGraphic(gearIV);
+    	exitButton.setTranslateX(450);
+        exitButton.setTranslateY(-375);
         exitButton.setOnAction(e -> view.switchPage(PagesEnum.PreviousScreen));
+        layout.getChildren().add(exitButton);
+    }
+    
+    public void adjBudgetTF()
+    {
+    	Label adjBudget = new Label("Adjust Budget:");
+    	adjBudget.setTranslateX(-150);
+        adjBudget.setTranslateY(-50);
+    	TextField adjBudjgettf = new TextField();
+    	adjBudjgettf.setTranslateX(0);
+        adjBudjgettf.setTranslateY(-50);
+        adjBudjgettf.setMaxWidth(100);
+        layout.getChildren().addAll(adjBudget, adjBudjgettf);
     }
 
-    public void save() {
+    public void changeTheme()
+    {
+    	Label theme = new Label("Change Theme:");
+    	theme.setTranslateX(-150);
+        theme.setTranslateY(0);
+        ComboBox<String> themeDL = new ComboBox<String>();
+        themeDL.getItems().add("Dark Mode");
+        themeDL.getItems().add("Light Mode");
+        themeDL.setPromptText("Theme");
+        themeDL.setMinWidth(100);
+        themeDL.setTranslateX(0);
+        themeDL.setTranslateY(0);
+        layout.getChildren().addAll(theme, themeDL);
     }
+
+    public void saveButton() 
+    {
+    	Button saveButton = new Button("Save Settings");
+    	saveButton.setTranslateX(-125);
+        saveButton.setTranslateY(150);
+        layout.getChildren().add(saveButton);
+    }
+    
+    public void finish()
+    {
+    	Button finishButton = new Button("Finish Garden");
+    	finishButton.setTranslateX(125);
+    	finishButton.setTranslateY(150);
+        layout.getChildren().add(finishButton);
+    }
+    
 }
