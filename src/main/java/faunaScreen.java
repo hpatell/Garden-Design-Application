67ce338a19d54455fcfacc1b35dfef51ff0acbc9
kushhp <kushhp@udel.edu>
2021-04-28 20:49:46 -0400

    fauna screen updated with new design logic, need to update table once model has the data for the plants

--- src/main/java/faunaScreen.java
+++ src/main/java/faunaScreen.java
@@ -1,11 +1,20 @@
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.image.Image;
 
@@ -13,88 +22,63 @@ public class faunaScreen extends Screen {
 	
 	public void updatePage(){}
 	
-	Text plantstext = new Text("Plants In Garden");
-	Text lepstext = new Text("Leps");
-	Text plantsimgtext = new Text("Image of Plants In Garden");
+	Text plantsText;
+	Text lepsText;
+	Text plantsImgText;
 	
-	Text FragariaStrawberry = new Text("Fragaria (Strawberry)");
-	Text HelianthusSunflower = new Text("Helianthus (Sunflower)");
-	Text SalixWillow = new Text("Salix (Willow)");
+	TableView plantsDataTable;
+
+	HBox hboxtop;
+	HBox hboxbottom;
+	BorderPane borderPane;
+	GridPane gridPane;
+	StackPane hstackPaneTop;
+	StackPane hstackPaneBottom;
 	
-	Text FragariaStrawberrylep = new Text("63");
-	Text HelianthusSunflowerlep = new Text("57");
-	Text SalixWillowlep = new Text("292");
-	
-	Image img1 = new Image("/FragariaStrawberry.png");
-	Image img2 = new Image("/HelianthusSunflower.png");
-	Image img3 = new Image("/SalixWillow.png");
-	
-	ImageView FragariaStrawberryimg = new ImageView(img1);
-	ImageView HelianthusSunflowerimg = new ImageView(img2);
-	ImageView SalixWillowimg = new ImageView(img3);
+	Model model;
 	
 	public faunaScreen(View v) {
 		super(v, PagesEnum.FaunaScreen);
 		
-		alignText();
+		model = new Model();
+		
+		borderPane = new BorderPane();	
+		gridPane = new GridPane();
+		plantsDataTable = new TableView();
+		hstackPaneTop = new StackPane();
+		hstackPaneBottom = new StackPane();	
+
+    	borderPane.setTop(hstackPaneTop);
+    	borderPane.setCenter(plantsDataTable);
+    	borderPane.setBottom(hstackPaneBottom);
+
+    	hboxtop = new HBox();
+    	hboxtop.setPadding(new Insets(10, 10, 0, 0));
+    	hboxtop.setAlignment(Pos.TOP_RIGHT);
+    	
+    	hboxbottom = new HBox();
+    	hboxbottom.setAlignment(Pos.TOP_CENTER);
+    	
 		createSettingsButton();
 		createReturnToPlotButton();
 		
-		layout = new StackPane();	
-		layout.getChildren().addAll(settingsButton, returnToPlotButton, HelianthusSunflowerimg, SalixWillowimg, FragariaStrawberryimg, plantstext, 
-    			lepstext, FragariaStrawberry, FragariaStrawberrylep, HelianthusSunflower, HelianthusSunflowerlep, SalixWillowlep, SalixWillow);	
-		layout.setStyle(theme);	
+		plantsText = new Text("Plants In Garden");
+		lepsText = new Text("Leps");
+		plantsImgText = new Text("Image of Plants In Garden");		
+		
+		gridPane.setGridLinesVisible(true);
+
+		
+		hboxtop.getChildren().addAll(settingsButton);
+		hboxbottom.getChildren().addAll(returnToPlotButton);
+		
+		hstackPaneTop.getChildren().addAll(hboxtop);
+		hstackPaneBottom.getChildren().addAll(hboxbottom);
+		
+		
+		borderPane.setStyle(theme);	
+		layout = borderPane;
 		scene = new Scene(layout, canvasWidth, canvasHeight);
 	}
-	
-	public void alignText() {
-		FragariaStrawberryimg.setPreserveRatio(true);
-		HelianthusSunflowerimg.setPreserveRatio(true);
-		SalixWillowimg.setPreserveRatio(true);
-		
-		FragariaStrawberryimg.setFitHeight(50);
-		FragariaStrawberryimg.setFitWidth(50);
-		
-		HelianthusSunflowerimg.setFitHeight(50);
-		HelianthusSunflowerimg.setFitWidth(50);
-		
-		SalixWillowimg.setFitHeight(50);
-		SalixWillowimg.setFitWidth(50);
-		
-		FragariaStrawberryimg.setTranslateX(-250);
-		FragariaStrawberryimg.setTranslateY(-50);
-		
-		HelianthusSunflowerimg.setTranslateX(-250);
-		HelianthusSunflowerimg.setTranslateY(0);
-		
-		SalixWillowimg.setTranslateX(-250);
-		SalixWillowimg.setTranslateY(50);
-    	
-    	plantsimgtext.setTranslateX(-200);
-    	plantsimgtext.setTranslateY(-100);
-    	
-    	plantstext.setTranslateX(-150);
-    	plantstext.setTranslateY(-100);
-    	
-    	lepstext.setTranslateX(75);
-    	lepstext.setTranslateY(-100);
-    	
-    	FragariaStrawberry.setTranslateX(-150);
-    	FragariaStrawberry.setTranslateY(-50);
-    	
-    	FragariaStrawberrylep.setTranslateX(75);
-    	FragariaStrawberrylep.setTranslateY(-50);
-    	
-    	HelianthusSunflower.setTranslateX(-150);
-    	HelianthusSunflower.setTranslateY(0);
-    	
-    	HelianthusSunflowerlep.setTranslateX(75);
-    	HelianthusSunflowerlep.setTranslateY(0);
-    	
-    	SalixWillow.setTranslateX(-150);
-    	SalixWillow.setTranslateY(50);
-    	
-    	SalixWillowlep.setTranslateX(75);
-    	SalixWillowlep.setTranslateY(50);
-	}
 }
+	
